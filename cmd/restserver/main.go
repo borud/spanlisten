15e37ac64f053708a5b56404ede82b4f705ba3c2
Bjorn Borud <borud@lab5e.org>
2021-05-11 12:17:23 +0200

    Added static file serving

--- cmd/restserver/main.go
+++ cmd/restserver/main.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello there %s\n", r.RemoteAddr)
+	fmt.Fprintf(w, "<h1>hello there %s</h1>\n", r.RemoteAddr)
+	fmt.Fprint(w, "<img width=200 src=\"/static/gopher.svg\">\n")
 }
 
 func main() {
 	mux := http.NewServeMux()
+
+	// Serve static files from the filesystem
+	fs := http.FileServer(http.Dir("./static"))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handle this with our indexhandler
 	mux.HandleFunc("/", indexHandler)
 
+	// Set up the server
 	server := http.Server{
-		Addr:    ":9090",
-		Handler: mux,
+		Addr:     ":9090",
+		Handler:  mux,
+		ErrorLog: log.Default(),
 	}
 
+	// Start the server
 	server.ListenAndServe()
 }
