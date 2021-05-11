1eb53eb9b996960bc617944c7ffa88adecd16ab4
Bjorn Borud <borud@lab5e.org>
2021-05-11 12:58:50 +0200

    query parameters

--- cmd/restserver/main.go
+++ cmd/restserver/main.go
@@ -11,6 +11,23 @@ import (
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>hello there %s</h1>\n", r.RemoteAddr)
 	fmt.Fprint(w, "<img width=200 src=\"/files/gopher.svg\">\n")
+
+	// Print query string
+	fmt.Fprintf(w, "<p>Query: %s</p>", r.URL.Query())
+
+	// Print key value pairs
+	fmt.Fprint(w, "<ul>\n")
+	for k, v := range r.URL.Query() {
+		fmt.Fprintf(w, "<li>%s = '%s'</li>", k, v)
+	}
+	fmt.Fprint(w, "</ul>\n")
+
+	// Picking out particular values
+	names := r.URL.Query()["name"]
+
+	if names != nil {
+		fmt.Fprintf(w, "The names are %s", names)
+	}
 }
 
 func main() {
@@ -22,8 +39,6 @@ func main() {
 
 	// Handle root with our indexHandler
 	mux.HandleFunc("/", indexHandler)
-
-	log.Printf("%#v", mux)
 
 	// Set up the server
 	server := http.Server{
